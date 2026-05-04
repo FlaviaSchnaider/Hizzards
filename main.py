e2baff754a2991c6c06d8bf583fc9fa3608bbec2
Flavia Schnaider <schflavia@hotmail.com>
2026-05-04 19:16:11 -0300

    leitura do arquivo .hex linha por linha, com arquivo .hex para teste

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ def interpretar_instrucao(inst_hex):
 
 def main():
     if len(sys.argv) < 2:
-        print("Uso: python riscv_hazards.py <arquivo.hex>")
+        print("Uso: python main.py <arquivo.hex>")
         sys.exit(1)
+
+    # leitura do arquivo .hex
+    arquivo = sys.argv[1]
+    instrucoes = []
+
+    with open(arquivo, 'r') as f:
+        for linha in f:
+            linha = linha.strip()
+            if not linha or linha.startswith('#'):
+                continue
+
+            instrucoes.append(linha)
+
+    print(instrucoes)
 
 if __name__ == '__main__':
     main()
